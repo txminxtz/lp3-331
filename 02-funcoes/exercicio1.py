429791b5b9152e44fb5b81c8c630dbb845993cc3
Taminato <126599638+txminxtz@users.noreply.github.com>
2024-03-17 14:06:22 -0300

    Delete 02-funcoes directory

--- 02-funcoes/exercicio1.py
+++
@@ -1,8 +0,0 @@
-# Programa mostra antecessor e sucessor
-
-numero = input("Digite um numero: ")
-
-antecessor = int(numero)-1 
-sucessor = int(numero)+1 
-
-print("Antecessor =", antecessor, "Sucessor =", sucessor)
